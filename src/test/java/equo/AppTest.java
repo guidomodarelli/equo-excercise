5874e311faf0f303d263dc557c03818a6f4e64e7
Guido Modarelli <guidomodarell25@gmail.com>
2021-06-17 11:15:24 -0300

    update tests

--- src/test/java/equo/AppTest.java
+++ src/test/java/equo/AppTest.java
@@ -1,27 +1,102 @@
 package equo;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.Test;
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 public class AppTest {
 
   @Test
-  public void testDron1() {
+  public void test1() {
+    Dron dron = new Dron(1, 2, 'N', "LMLMLMLMM");
+    Plateau plateau = new Plateau(5, 5);
+    dron.explore(plateau);
+
+    assertEquals("1 3 N", dron.toString());
+  }
+
+  @Test
+  public void test2() {
     Dron dron = new Dron(3, 3, 'E', "MMRMMRMRRM");
     Plateau plateau = new Plateau(5, 5);
+
     dron.explore(plateau);
 
     assertEquals("5 1 E", dron.toString());
   }
 
   @Test
-  public void testDron2() {
-    Dron dron = new Dron(1, 2, 'N', "LMLMLMLMM");
+  public void test3() {
+    Dron dron = new Dron(0, 0, 'N', "MMMRM");
     Plateau plateau = new Plateau(5, 5);
 
     dron.explore(plateau);
 
-    assertEquals("1 3 N", dron.toString());
+    assertEquals("1 3 E", dron.toString());
+  }
+
+  @Test
+  @DisplayName("Colision entre drones")
+  public void testIntegration1() {
+    ArrayList<Dron> squat = new ArrayList<Dron>() {
+      {
+        add(new Dron(1, 2, 'N', "LMLMLMLMM"));
+        add(new Dron(0, 0, 'N', "MMMRM"));
+      }
+    };
+    Plateau plateau = new Plateau(5, 5, squat);
+
+    try {
+      Dron dron = squat.get(0);
+      dron.explore(plateau);
+      dron = squat.get(1);
+      dron.explore(plateau);
+      throw new Error("No hay colision!");
+    } catch (Throwable err) {
+      assertEquals(Plateau.COLISION, err.getMessage());
+    }
+  }
+
+  @Test
+  @DisplayName("Instrucción invalida")
+  public void test4() {
+    try {
+      new Dron(0, 0, 'N', "MLRE");
+    } catch (Throwable err) {
+      assertEquals(Dron.INVALID_INSTRUCTION, err.getMessage());
+    }
+  }
+
+  @Test
+  @DisplayName("Orientación invalida")
+  public void test5() {
+    try {
+      new Dron(0, 0, 'T', "MLR");
+    } catch (Throwable err) {
+      assertEquals(Dron.INVALID_ORIENTATION, err.getMessage());
+    }
+  }
+
+  @Test
+  @DisplayName("Ancho de la meseta mal definido")
+  public void test6() {
+    try {
+      new Plateau(0, 1);
+    } catch (Throwable err) {
+      assertEquals(Plateau.INVALID_WIDTH, err.getMessage());
+    }
+  }
+
+  @Test
+  @DisplayName("Altura de la meseta mal definido")
+  public void test7() {
+    try {
+      new Plateau(1, 0);
+    } catch (Throwable err) {
+      assertEquals(Plateau.INVALID_HEIGTH, err.getMessage());
+    }
   }
 }
