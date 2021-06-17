2f59ce26163dcb9cc7679d5a7d406fa02da5428c
Guido Modarelli <guidomodarell25@gmail.com>
2021-06-17 15:27:47 -0300

    add app ppal

--- src/main/java/equo/App.java
+++ src/main/java/equo/App.java
@@ -1,11 +1,30 @@
 package equo;
 
-public class App {
-  public static void main(String[] args) {
-    Dron dron = new Dron(3, 3, 'E', "MMRMMRMRRM");
-    Plateau plateau = new Plateau(5, 5);
-    dron.explore(plateau);
+import java.util.ArrayList;
 
-    System.out.println(dron);
+public class App {
+
+  private Plateau plateau;
+  private ArrayList<Dron> squat;
+
+  App(String dimensions, ArrayList<String> list) throws Error {
+    this.plateau = new Plateau(dimensions);
+    this.squat = new ArrayList<>();
+
+    for (int i = 0; i < list.size(); i++) {
+      String position = list.get(i);
+      String instructions = null;
+      if (i < list.size() - 1) {
+        instructions = list.get(++i);
+      }
+      this.squat.add(new Dron(position, instructions));
+    }
+  }
+
+  public void resolve() throws Error {
+    for (Dron dron : squat) {
+      dron.explore(plateau);
+      System.out.println(dron);
+    }
   }
 }
